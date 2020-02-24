166e144529168a0419a61d8b56944153f35a7332
fotis tsakiris <footios76@gmail.com>
2020-02-24 11:37:08 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import CodeInitial from "./src/CodeInitial";
+import CodeExperiment from "./src/CodeExperiment";
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <CodeExperiment />
+      <CodeInitial />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
